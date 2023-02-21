be80e4e2a1dc79732e80f5e793fe5df038bdc9f8
dpv927 <dpv927@inlumine.ual.es>
2023-02-21 05:33:37 -0500

    Documentation

--- board.h
+++ board.h
@@ -1,24 +1,28 @@
+#include "player.h"
 #define BOARD_LEN 9
-#define PLAYER_1 1
-#define PLAYER_2 2
-#define NONE 0
 
-/** @brief Returns 0 if the board its not full, 1 if it is */
+/** @brief Returns 0 if the board it not full, 1 if it is.
+ * @param board Array that represents the game board */
 int boardIsFull(int board[]) {
-  for (int i=0; i<BOARD_LEN; i++)
-	  if (board[i] == NONE)
+  for (int i=0; i< BOARD_LEN; i++)
+	  if (board[i] == PLAYER_NONE)
 		  return 0;
 	return 1;
 }
 
-/** @brief Returns -1 if player 2 wins, 1 if player 1 does and 
- * 0 if no player wins at this point */
+/** @brief Evaluates a a group of consecutive cells from
+ * a board.
+ * @param start Start index of a cell in the board.
+ * @param step In how many cells is the next consecutive cells.
+ * @param board Array that represents the game board.
+ * @returns -1 if player 2 wins, 1 if player 1 does and 
+ * 0 if no player wins at this point. */
 int evaluateInStep(int start, int step, int board[]) {
  int pre_owner, end = start + step;
 
   for (int i = start; i <= end; i += step) {
 			pre_owner = board[i-step];
-			if (pre_owner == NONE || board[i] != pre_owner)
+			if (pre_owner == PLAYER_NONE || board[i] != pre_owner)
 				return 0;
 			if (i == end)
 				return (pre_owner == PLAYER_1)? 1 : -1;
@@ -26,9 +30,11 @@ int evaluateInStep(int start, int step, int board[]) {
 		return 0;
 }
 
-/* @brief Returns -1 if player2 wins, 1 if player1 does, 0 if 
+/* @brief Evaluates the actual state of a board.
+ * @param board Array that represents the game board.
+ * @return -1 if player2 wins, 1 if player1 does, 0 if 
  * there is a tie and -2 if no one wins and there are more
- * posible moves for the players.*/
+ * posible moves for the players. */
 int evaluateGame(int board[]) {
   // Diagonal left up - right down
 		int result = evaluateInStep(4, 4, board);
