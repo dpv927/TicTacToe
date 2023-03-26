c5f81451b4ac5b52c5f42fc5966467089f011ed2
dpv927 <dpv927@inlumine.ual.es>
2023-03-26 13:26:20 +0200

    Rename

--- src/game.h
+++
@@ -1,6 +0,0 @@
-#define PLAYER_1  -100 // Player 1 ID
-#define PLAYER_2  100  // Player 2 ID
-#define PLAYER_N  0    // Player 'none' ID
-#define COND_DRAW 0    // Draw game condition
-#define COND_KEEP 1    // Keep playing condition
-#define BOARD_LEN 9    // Board length
