7d05af6b896dc664ff0d7a34e6bbd0dd817c03e9
dpv927 <dpv927@inlumine.ual.es>
2023-04-06 20:44:56 +0200

    Update constants

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,20 @@
-#define PLAYER_1  -100 /* Player 1 ID */
-#define PLAYER_2  100  /* Player 2 ID */
-#define PLAYER_N  0    /* Player 'none' ID */
-#define COND_DRAW 0    /* Draw game condition */
-#define COND_KEEP 1    /* Keep playing condition */
-#define BOARD_LEN 9    /* Board length */
+/* general settings */
+static const int PLAYER_1  = 1;
+static const int PLAYER_2  = 2;
+static const int PLAYER_N  = 0;
+static const int COND_DRAW = 0;
+static const int COND_KEEP = 1;
+static const unsigned int BOARD_LEN = 9;
+
+/* appearance */
+static const char P1_NAME[] = "Player1";
+static const char P2_NAME[] = "Player2";
+
+static const char P1_ALIAS = 'x';
+static const char P2_ALIAS = 'o'; 
+
+static const char BOARD_COLOR[]    = "\033[1;31m";
+static const char ANSI_RESET[]   = "\033[1;37m";
+
+/* search settings */
+static const unsigned int DEF_DEPTH = 9;
