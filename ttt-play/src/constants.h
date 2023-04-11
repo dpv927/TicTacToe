b589c7fcde5f4b4b81ae70e329165f0024826caa
dpv927 <dpv927@inlumine.ual.es>
2023-04-11 11:50:23 +0200

    Move to .

--- ttt-play/src/constants.h
+++
@@ -1,7 +0,0 @@
-/* general settings */
-static const int PLAYER_1  = 1;
-static const int PLAYER_2  = 2;
-static const int PLAYER_N  = 0;
-static const int COND_DRAW = 0;
-static const int COND_KEEP = 1;
-static const int BOARD_LEN = 9;
--- ttt-play/src/resources.h
+++
@@ -1,5 +0,0 @@
-/**
- * @brief Prints the content of a documentation file.
- * @param poth Path of the file whe is the documentation.
- * **/
-void printdoc(char* path);
