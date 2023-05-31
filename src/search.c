c0d486a2f99204438acd9be0a5ba814ff529b7ec
dpv927 <dpv927@inlumine.ual.es>
2023-06-01 01:32:17 +0200

    Add new heuristic function

--- src/search.c
+++ src/search.c
@@ -1,16 +1,19 @@
 #include <limits.h>
 #include "search.h"
+#include "game_info.h"
 #include "board_info.h"
-#include "player.h"
+#include "board.h"
 #include "heuristic.h"
 
 int minimax(int* board, int is_maximizing, int pmaxId, int pminId) {
+  int eval = heuristic(board, pmaxId); 
+  if(eval != EVAL_KEEP /*|| depth == maxdepth*/) { // Terminal node
+    return (eval == MAX_EVAL)? MAX_EVAL /* -depth */ :
+      ((eval == MIN_EVAL)? MIN_EVAL /* +depth */ : NO_EVAL);
+  }
+
   int score;
   int best_score;
-
-  score = heuristic(board, pmaxId);
-  if(score == MAX_EVAL || score == MIN_EVAL || score == NO_EVAL)
-    return score;
 
   if(is_maximizing) {
     best_score = INT_MIN;
--- src/search.h
+++ src/search.h
@@ -1,3 +1,4 @@
+#pragma once
 /** Macros **/
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))
