2464773a48a0bc31c0a0ec5fc02b81a375ffe11f
dpv927 <dpv927@inlumine.ual.es>
2023-05-31 17:56:50 +0200

    Update and add heuristic

--- src/search.c
+++ src/search.c
@@ -1,22 +1,24 @@
 #include <limits.h>
-#include "constants.h"
-#include "board.h"
 #include "search.h"
+#include "board_info.h"
+#include "player.h"
+#include "heuristic.h"
 
-int minimax(int board[], int is_maximizing, int player_id) {
+int minimax(int* board, int is_maximizing, int pmaxId, int pminId) {
   int score;
   int best_score;
 
-  score = evaluateGame(board, player_id);
-  if(score != COND_KEEP) return score;
+  score = heuristic(board, pmaxId);
+  if(score == MAX_EVAL || score == MIN_EVAL || score == NO_EVAL)
+    return score;
 
   if(is_maximizing) {
     best_score = INT_MIN;
     
     for (int i = 0; i < BOARD_LEN; i++) {
       if(board[i] == PLAYER_N) {
-        board[i] = PLAYER_2;
-        score = minimax(board, 0, player_id);
+        board[i] = pmaxId;
+        score = minimax(board, 0, pmaxId, pminId);
         board[i] = PLAYER_N;
         best_score = max(score, best_score);
       }
@@ -27,8 +29,8 @@ int minimax(int board[], int is_maximizing, int player_id) {
 
     for (int i = 0; i < BOARD_LEN; i++) {
       if (board[i] == PLAYER_N) {
-        board[i] = PLAYER_1;
-        score = minimax(board, 1, player_id);
+        board[i] = pminId;
+        score = minimax(board, 1, pmaxId, pminId);
         board[i] = PLAYER_N;
         best_score = min(score, best_score);
       }
--- src/search.h
+++ src/search.h
@@ -4,11 +4,12 @@
 
 /**
  * @brief Calculates the minimax algorithm over a board. Depending if the 
- * algorithm is maximizing returns the best or worst possible move for the `ñayer to move.
- * 
+ * algorithm is maximizing returns the best or worst possible move for the player to move.
+ *
  * @param board Game board.
- * @param is_maximizing Determines if the algorithm has to maximize the move. (True (1) = best,
- * False (0) = worst.
- * @param player ID of the player to perform the move.
- * **/
-int minimax(int board[], int is_maximizing, int player_id);
+ * @param is_maximizing Determines if the algorithm has to maximize the move.
+ * @param pmaxId Id of the player to maximize.
+ * @param pminId Id of the player to minimize. 
+ * 
+ * @returns Index of the best calculated move. **/
+int minimax(int* board, int is_maximizing, int pmaxId, int pminId);
