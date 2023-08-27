b4ddd353b9b814062afd316c7bfed5ffb1afd784
dpv927 <dpv927@inlumine.ual.es>
2023-08-27 22:31:51 +0200

    Add analisis module

--- src/ttt.c
+++ src/ttt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "analysis.h"
 #include "resources.h"
 #include "game.h"
 
@@ -32,11 +33,34 @@ int main(int argc, char *argv[]) {
 \033[0mUnrecognized option: %s\nexiting...\n", argv[1]);
     }
   }
+  else if(argc == 3) {
+    if(strcmp(argv[1], "--search") == 0) {
+       // call analysis func with argv[2] as FEN
+    }else {
+      printf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0mUnrecognized option: %s\nexiting...\n", argv[1]);
+    }
+  }
   else if(argc == 4) {
     if(strcmp(argv[1], "--ai-mode") == 0) {
       if(strcmp(argv[2], "--max-depth") == 0) {
         start_game(0, atoi(argv[3]));
       } 
+    }
+  }
+  else if(argc == 5) {
+    if(strcmp(argv[1], "--search") == 0) {
+      if(strcmp(argv[3], "--depth") == 0) {
+        // call analysis func with argv[2] as FEN and argv[4]
+        // as depth.
+      }
+      else {
+        printf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0mUnrecognized option: %s\nexiting...\n", argv[3]);
+      }
+    }else {
+      printf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0mUnrecognized option: %s\nexiting...\n", argv[1]);
     }
   }
   return 0;
