00c4835d1959338d54224b16fdca6f4f2024340c
dpv927 <dpv927@inlumine.ual.es>
2023-10-17 09:16:18 +0000

    New way to handle arguments

--- src/argshand.c
+++ src/argshand.c
@@ -1,15 +1,131 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "argshand.h"
+#include "game.h"
+#include "resources.h"
+#include "sync_game.h"
+#include "analysis.h"
 
-char* GetArg(char const *str_to_find, int narg, char **arg) {
-  for(short i=0;i<narg;i++)                
-    if (!strcmp(arg[i], str_to_find)) 
-      return arg[i+1];          
-  return NULL;           
+void CheckNumberArgs(int argc) {
+  if(argc < MIN_NUM_ARG) {
+    fprintf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0mInvalid number of arguments (%d)!\nexiting...\n", argc-1);
+    exit(EXIT_FAILURE);
+  }
 }
 
-int ExistArg(char const *str_to_find, int narg, char **arg) {
+void BadArgument() {
+  fprintf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0mUnrecognized option/s or bad placement.\nexiting...\n");
+  exit(EXIT_FAILURE);
+}
+
+void BadArguments(const char* value,const char* parameter) {
+  fprintf(stderr, "\033[1;37mttt: \033[1;31mfatal error: \
+\033[0m%s: Not a value for the option '%s'.\nexiting...\n", value, parameter);
+  exit(EXIT_FAILURE);
+}
+
+void ProcessArguments(int argc, char** argv) {
+
+  /* ---- Version information ---- */
+  if(ExistsArg("--version", argc, argv) && argc == MIN_NUM_ARG) {
+    //printdoc("/etc/ttt/doc/version.txt");
+    printdoc("../doc/version.txt"); // testing path 
+    exit(EXIT_SUCCESS);}
+  
+  /* ---- Help message ---- */
+  if(ExistsArg("--help", argc, argv) && argc == MIN_NUM_ARG) {
+    //printdoc("/etc/ttt/doc/version.txt");
+    printdoc("../doc/help.txt"); // testing path 
+    exit(EXIT_SUCCESS);}
+
+
+  /* --- Argument info initialization ---  */
+  int param_index;
+  struct Search s_info = {9,0};
+  struct Versus vs_info = {9,9,Human,Human,0};
+  /* --- Argument info initialization ---  */
+
+
+  /* ---- Search Depth ---- */
+  if((param_index = ExistsArg("--search-depth", argc, argv))) {
+    if(param_index == argc-1) BadArgument();
+
+    char* endptr;
+    strtol(argv[param_index+1], &endptr, 10);
+
+    if(*endptr == '\0') { 
+      s_info.search_depth = atoi(argv[param_index+1]);
+      s_info.used = 1;
+    } else { BadArguments(argv[param_index+1], argv[param_index]); }
+  }
+
+  /* ---- Search Depth for Ai player1 (Only 1vs1) ---- */
+  if((param_index = ExistsArg("--depth-Ai1", argc, argv))) {
+    if(s_info.used || param_index == argc-1) BadArgument();
+    
+    char* endptr;
+    strtol(argv[param_index+1], &endptr, 10);
+
+    if(*endptr == '\0') { 
+      vs_info.search_depth_P1 = atoi(argv[param_index+1]);
+      vs_info.used = 1;
+    } else { BadArguments(argv[param_index+1], argv[param_index]); }
+  }
+
+  /* ---- Search Depth for Ai player2 (Only 1vs1) ---- */
+  if((param_index = ExistsArg("--depth-Ai2", argc, argv))) {
+    if(s_info.used || param_index == argc-1) BadArgument();
+
+    char* endptr;
+    strtol(argv[param_index+1], &endptr, 10);
+
+    if(*endptr == '\0') { 
+      vs_info.search_depth_P1 = atoi(argv[param_index+1]);
+      vs_info.used = 1;
+    } else { BadArguments(argv[param_index+1], argv[param_index]); }
+  }
+
+  /* ---- Type of player of player1 (Only 1vs1) ---- */
+  if((param_index = ExistsArg("--p1-type", argc, argv))) {
+    if(s_info.used || param_index == argc-1) BadArgument();
+
+    if(!strcmp(argv[param_index+1], "Human")) {
+      vs_info.p1_type = Human;
+      vs_info.used = 1;
+    } else if(!strcmp(argv[param_index+1], "Ai")) {
+      vs_info.p1_type = Ai;
+      vs_info.used = 1;
+    }else BadArguments(argv[param_index+1], argv[param_index]); 
+  }
+
+  /* ---- Type of player for player2 (Only 1vs1) ---- */
+  if((param_index = ExistsArg("--p2-type", argc, argv))) {
+    if(s_info.used || param_index == argc-1) BadArgument();
+
+    if(!strcmp(argv[param_index+1], "Human")) {
+      vs_info.p2_type = Human;
+      vs_info.used = 1;
+    } else if(!strcmp(argv[param_index+1], "Ai")) {
+      vs_info.p2_type = Ai;
+      vs_info.used = 1;
+    }else BadArguments(argv[param_index+1], argv[param_index]); 
+  }
+
+  /* ---- Start Sybcronyzed game? ---- */
+  if((ExistsArg("--versus", argc, argv))) {
+    if(s_info.used) BadArgument();
+    syncronized_game(vs_info.p1_type, vs_info.p2_type, vs_info.search_depth_P1, vs_info.search_depth_P2); }
+
+  /* ---- Start Best Move Search? ---- */
+  if((param_index = ExistsArg("--bestmove", argc, argv))) {
+    if(vs_info.used) BadArgument();
+    search(argv[param_index], s_info.search_depth); }
+}
+
+int ExistsArg(char const *str_to_find, int narg, char **arg) {
   for(short i=0;i<narg;i++)
     if (!strcmp(arg[i], str_to_find)) 
       return i;
--- src/argshand.h
+++ src/argshand.h
@@ -1,31 +1,49 @@
 #ifndef ARGSHAND_H
 #define ARGSHAND_H
 
-/* @brief Comprueba si una cadena se encuentra en el array de argumentos de
- * entrada del programa como parametro, y si es asi, devuelve la el siguiente
- * parametro.
- *
- * Un ejemplo puede ser cuando le mandamos al programa ./Bandera -n 300 y queremos
- * saber que parametro o valor hay depues del parmetro '-n'.
- *
- * @param str_to_find Argumento a encontrar
- * @param int numero de argumentos
- * @param arg argumentos del programa
- *
- * @return Siguiente argumentos al buscado. Si no se encuentra, devuelve NULL, 
- * puntero a 0.
- * */
-char *GetArg(char const *str_to_find, int narg, char **arg);
+#include <stdint.h>
+#include "game.h"
 
-/* @brief Comprueba si una cadena se encuentra en el array de argumentos de
- * entrada del programa como parametro.
- *
- * @param str_to_find Argumento a encontrar
- * @param int numero de argumentos
- * @param arg argumentos del programa
- *
- * @return Si se encuentra, devuelve 1, en caso contrario 0.
- * */
-int ExistArg(char const *str_to_find, int narg, char **arg);
+/* Min num. of arguments */
+#define MIN_NUM_ARG       2
+
+/* All the arguments */
+#define VERSION_ARG       "--version"
+#define HELP_ARG          "--help"
+#define SEARCH_DEPTH_ARG  "--search-depth"
+#define AI1_DEPTH_ARG     "--depth-Ai1"    
+#define AI2_DEPTH_ARG     "--depth-Ai2"    
+#define P1_TYPE_ARG       "--p1-type"    
+#define P2_TYPE_ARG       "--p2-type"
+#define VERSUS_ARG        "--versus"
+#define SEARCH_ARG        "--bestmove"
+
+struct Search {
+  uint8_t search_depth;
+  uint8_t used;
+};
+
+struct Versus {
+  uint8_t search_depth_P1;
+  uint8_t search_depth_P2;
+  enum PlayerType p1_type;
+  enum PlayerType p2_type;
+  uint8_t used;
+};
+
+/* Checks if an argument exists in the given parameters */
+int ExistsArg(char const *str_to_find, int narg, char **arg);
+
+/* Check the number of arguments */
+void CheckNumberArgs(int argc);
+
+/* Prints an error message due a bad argument */
+void BadArgument();
+
+/* Prints an error message due a wrong value for a parameter */
+void BadArguments(const char* value,const char* parameter);
+
+/* Process all the arguments and get info */
+void ProcessArguments(int argc, char** argv);
 
 #endif
